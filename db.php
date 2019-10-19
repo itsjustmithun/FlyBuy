721576507bfa75396f8b5c8e8d288953db8fab03
itsjustmithun <mitbusiness15@gmail.com>
2019-10-19 14:59:53 +0530

    database connectivity

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "itsjustmithun";
+$password = "root123";
+$db = "ecommerece";
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+?>s
